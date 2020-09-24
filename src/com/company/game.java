8a387fe341c911e8986991f1bd9929f8baf7e66b
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-09-24 14:08:15 -0700

    agregada clase game

---
+++ src/com/company/game.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class game {
+    ArrayList<String> colores = new ArrayList<>();
+    game(){
+        colores.add("rojo");
+        colores.add("azul");
+        colores.add("amarillo");
+        colores.add("verde");
+    }
+
+}

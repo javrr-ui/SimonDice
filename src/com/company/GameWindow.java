d4fb585b989cd99008976481d190f64b9d819d8f
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-09-26 14:22:36 -0700

    Agregados componentes visuales

--- src/com/company/GameWindow.java
+++ src/com/company/GameWindow.java
@@ -1,21 +1,81 @@
 package com.company;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
-public class GameWindow {
+public class GameWindow extends JFrame{
     private JButton comenzarJuegoButton;
     private JPanel mainPanel;
-    private JPanel buttonPanel;
-    private JButton verde;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JPanel leftPanel;
+    private JLabel green;
+    private JLabel red;
+    private JLabel yellow;
+    private JLabel blue;
+   private JLabel nextGreen;
+   private JLabel nextRed;
+   private JLabel nextYellow;
+   private JLabel nextBlue;
 
     public GameWindow() {
-        comenzarJuegoButton.addActionListener(e -> {
 
-        });
+        inicializarComponentes();
+
+        this.add(leftPanel);
+        this.add(mainPanel);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(900,480);
+        this.setVisible(true);
+    }
+
+    public void inicializarComponentes(){
+        mainPanel = new JPanel();
+        leftPanel = new JPanel();
+        green = new JLabel();
+        red = new JLabel();
+        yellow = new JLabel();
+        blue = new JLabel();
+        comenzarJuegoButton = new JButton("Comenzar juego");
+
+        //crea imageIcon a partir de una imagen y la reescala XD
+        ImageIcon blueIcon = new ImageIcon(new ImageIcon("./img/blue.png").getImage().getScaledInstance(180,180,Image.SCALE_SMOOTH));
+        ImageIcon redIcon = new ImageIcon(new ImageIcon("./img/red.png").getImage().getScaledInstance(180,180,Image.SCALE_SMOOTH));
+        ImageIcon yellowIcon = new ImageIcon(new ImageIcon("./img/yellow.png").getImage().getScaledInstance(180,180,Image.SCALE_SMOOTH));
+        ImageIcon greenIcon = new ImageIcon(new ImageIcon("./img/green.png").getImage().getScaledInstance(180,180,Image.SCALE_SMOOTH));
+
+        green.setIcon(greenIcon);
+        red.setIcon(redIcon);
+        yellow.setIcon(yellowIcon);
+        blue.setIcon(blueIcon);
+
+        //BOTONES
+
+
+
+
+        //main panel settings
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        mainPanel.add(comenzarJuegoButton,gbc);
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        mainPanel.add(green,gbc);
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        mainPanel.add(red,gbc);
+        gbc.gridx =1;
+        gbc.gridy =1;
+        mainPanel.add(yellow,gbc);
+        gbc.gridx =2;
+        gbc.gridy =1;
+        mainPanel.add(blue,gbc);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        GameWindow window = new GameWindow();
+
         Game game = new Game();
     }
 }

2c06c1172db9ce0416a115177b89096fcf620f0e
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-09-25 22:15:55 -0700

    Agregada la clase GameWindow para mostrar el juego de manera grafica.

---
+++ src/com/company/GameWindow.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow {
+    private JButton comenzarJuegoButton;
+    private JPanel mainPanel;
+
+    public GameWindow() {
+        comenzarJuegoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}

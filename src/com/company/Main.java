0ccffc38de5c95c38cecb995520862a0d8091b37
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-10-03 23:13:58 -0700

    #15 Agregado codigo para leer argumentos en la ejecucion, por si se desea ejecutar el programa en modo no-gui

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,19 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (IllegalAccessException|InstantiationException|UnsupportedLookAndFeelException|ClassNotFoundException e) {
-            e.printStackTrace();
+
+        if(args[0].equals("no-gui")){
+            Game game = new Game();
+            game.consoleMode();
+        }else{
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (IllegalAccessException|InstantiationException|UnsupportedLookAndFeelException|ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            GameWindow window = new GameWindow();
+            window.startGame();
         }
-        GameWindow window = new GameWindow();
+
     }
 }

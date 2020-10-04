023409b1112e53f5b4872213836d32af340afdcb
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-10-03 21:02:28 -0700

    #14 Catching Multiple Exception Types and Rethrowing Exceptions with Improved Type Checking

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IllegalAccessException|InstantiationException|UnsupportedLookAndFeelException|ClassNotFoundException e) {
             e.printStackTrace();
         }
         GameWindow window = new GameWindow();

6105ed3aa3de78a253aaec41a5cf47a934543235
javrr-ui <67305880+javrr-ui@users.noreply.github.com>
2020-10-08 17:23:12 -0700

    (#21) Clase renombrada porque genera problemas con una clase existente de Java XD

--- src/com/company/Colors.java
+++ src/com/company/Colors.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Random;
 
-public class Color {
+public class Colors {
     private static String[] colores ={"verde","rojo","azul","amarillo"};
     private static Random rand = new Random();
 

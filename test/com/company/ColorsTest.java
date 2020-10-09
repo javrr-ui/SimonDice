5283e7bc8cc10908d1cf7c69665bfbbef24eb050
javatlacati <scherzo_16@hotmail.com>
2020-10-09 18:53:48 -0500

    Demostración simple de prueba unitaria

---
+++ test/com/company/ColorsTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ColorsTest {
+
+    @Test
+    public void getRandomColor() {
+        String randomColor = Colors.getRandomColor();
+        assertNotNull(randomColor);
+    }
+}

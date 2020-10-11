4490ff08338c4a131959988de28a67088a5844e0
javatlacati <scherzo_16@hotmail.com>
2020-10-10 22:02:53 -0500

    avances en el happy path

--- src/test/java/com/company/GameTest.java
+++ src/test/java/com/company/GameTest.java
@@ -3,6 +3,7 @@ package com.company;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class GameTest {
@@ -10,14 +11,20 @@ public class GameTest {
     @Test
     public void elJuegoNoComienzaHastaQueComienza() {
         Game game = new Game();
-        assertFalse( "El juego no debería estar como iniciado si no lo hemos iniciado explícitamente",game.haEmpezadoJuego());
+        assertFalse("El juego no debería estar como iniciado si no lo hemos iniciado explícitamente", game.haEmpezadoJuego());
         game.startWindowMode();
-        assertTrue( "El juego debería estar iniciado tras tratar de iniciarlo",game.haEmpezadoJuego());
-        //game.restart();
-        assertTrue( "reiniciar el juego debería de dejarlo iniciado",game.haEmpezadoJuego());
-        Game game1 = new Game();
-        assertFalse( "El juego no debería estar como iniciado si no lo hemos iniciado explícitamente",game1.haEmpezadoJuego());
-        //game1.restart();
-        //assertTrue(game1.haEmpezadoJuego(), "reiniciar el juego al inicio debería de dejarlo iniciado");
+        assertTrue("El juego debería estar iniciado tras tratar de iniciarlo", game.haEmpezadoJuego());
+    }
+
+    @Test
+    public void happyPath() {
+        Game game = new Game();
+        assertFalse("El juego no debería estar como iniciado si no lo hemos iniciado explícitamente", game.haEmpezadoJuego());
+        game.startJuego();
+        assertTrue("El juego debería estar iniciado tras tratar de iniciarlo", game.haEmpezadoJuego());
+        assertTrue(game.isJuegoEnCurso());
+        assertTrue(game.tamanoStackIguales());
+        assertNotNull(game.getSecuencia());
+        assertTrue(game.getSecuencia().empty());
     }
 }

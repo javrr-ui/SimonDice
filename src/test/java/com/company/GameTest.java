8ae1c22038cd3ead35d400aa809c48b2a5f6f05e
javatlacati <scherzo_16@hotmail.com>
2020-10-18 23:50:39 -0500

    Trying some tricks for the sexit part :3

--- src/test/java/com/company/GameTest.java
+++ src/test/java/com/company/GameTest.java
@@ -1,12 +1,22 @@
 package com.company;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.StringBufferInputStream;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class GameTest {
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Test
     public void elJuegoNoComienzaHastaQueComienza() {
@@ -30,15 +40,23 @@ public class GameTest {
 
     @Test
     public void consoleMode() {
+        InputStream viejaentrada = System.in;
+        StringBufferInputStream nuevaentrada = new StringBufferInputStream("4\n");
+        System.setIn(nuevaentrada);
 
-        //viendo que funcione ._.XD
+        ByteArrayOutputStream salidaBinaria = new ByteArrayOutputStream();
+        PrintStream salida = new PrintStream(salidaBinaria);
+        PrintStream viejaSalida = System.out;
+        // Use my stream
+        System.setOut(salida);
+
         Game game = new Game();
-        assertFalse("El juego no debería estar como iniciado si no lo hemos iniciado explícitamente", game.haEmpezadoJuego());
+        exit.expectSystemExitWithStatus(0);
         game.consoleMode();
-        assertTrue("El juego debería estar iniciado tras tratar de iniciarlo", game.haEmpezadoJuego());
-        assertTrue(game.isJuegoEnCurso());
-        assertTrue(game.tamanoStackIguales());
-        assertNotNull(game.getSecuencia());
-        assertTrue(game.getSecuencia().empty());
+
+        // Put things back
+        System.out.flush();
+        System.setOut(viejaSalida);
+        System.setIn(viejaentrada);
     }
 }

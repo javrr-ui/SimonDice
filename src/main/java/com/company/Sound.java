2e28f23cc11fac8f31ba75348649a669b938884e
Javier <framirez76@uabc.edu.mx>
2021-04-03 18:28:04 -0700

    Update Sound.java
    arreglado error, ya se reproduce el sonido al presionar un boton

--- src/main/java/com/company/Sound.java
+++ src/main/java/com/company/Sound.java
@@ -5,62 +5,55 @@
  */
 package com.company;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-  
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioFormat;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  *
  * @author Javi
  */
 public class Sound {
-    
-    
+
     private Clip clip;
-    
+
     private AudioInputStream audioInputStream;
-    private String filePath;
-    private URL file = ClassLoader.getSystemResource("soundEffects/click.wav");
-    private URI uri;
-    public Sound() throws UnsupportedAudioFileException,IOException, LineUnavailableException{
-        
-        try{
-            uri = file.toURI();
-        }catch(URISyntaxException e){
-            System.out.println("URI Syntax Error: " + e.getMessage());
-        }
-        
-        //create audioInputStream object
-        audioInputStream = AudioSystem.getAudioInputStream(new File(uri).getAbsoluteFile());
-        
+    private InputStream inputStream;
+    long inicio;
+
+    public Sound() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        //read audio data from whatever source (file/classloader/etc.)
+        inputStream = Main.class.getClassLoader().getResourceAsStream("soundEffects\\click.au");
+
+        //audio input stream
+        audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+
         //create clip reference
         clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+    }
+
+    public void play() {
+
         
-        //open audioInputStream to the clip
-        clip.open(audioInputStream);
-        //clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
-    
-    public void play() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
-        clip.open(audioInputStream);
+        clip.setMicrosecondPosition(0);
         clip.start();
-        resetAudioStream();
-        clip.close();
+
     }
-    
-    public void resetAudioStream()throws UnsupportedAudioFileException,IOException, LineUnavailableException{
-       
-        audioInputStream = AudioSystem.getAudioInputStream(new File(uri).getAbsoluteFile());
-        clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-       // clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
-    
+
 }

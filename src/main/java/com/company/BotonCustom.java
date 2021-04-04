63368e2d919147a8249c95d803db2168e713f87e
Javier <framirez76@uabc.edu.mx>
2021-04-04 10:49:54 -0700

    Update BotonCustom.java

--- src/main/java/com/company/BotonCustom.java
+++ src/main/java/com/company/BotonCustom.java
@@ -27,9 +27,10 @@ public class BotonCustom extends JButton {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setPaint(new GradientPaint(new Point(0, 0), Color.WHITE, new Point(0, getHeight()), Color.PINK.darker()));
+        g2d.setPaint(backgroundColor);
         g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 30, 30);
         g2d.setPaint(Color.BLACK);
-        g2d.drawString(getText(), 30, 12);
+        g2d.drawString(text, 30, 12);
         g2d.dispose();
     }
 
